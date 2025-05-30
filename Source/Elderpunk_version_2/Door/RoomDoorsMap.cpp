8f766ef12fabbfa6dcb8fc4b64a16c4c85586d30
Aleksandra Nosińska <aleks.nosinska@gmail.com>
2025-05-30 23:28:45 +0200

    Add mechanic teleport

---
+++ Source/Elderpunk_version_2/Door/RoomDoorsMap.cpp
@@ -0,0 +1,26 @@
+﻿#include "RoomDoorsMap.h"
+
+FDoor ARoomDoorsMap::GetRandomRoomDoors(EDirection exitDirection)
+{
+	int maxRandom = RoomDoors.Num() - 1;
+	int rounds = 0, maxRounds = 20;
+	
+	while (rounds++ < maxRounds) {
+		int idRandomRoom = FMath::RandRange(0,maxRandom-1);
+		EDirection reverseDir = mapReverseDirection[exitDirection];
+
+		if (idRandomRoom == idCurrentRoom) {
+			continue;
+		}
+		
+		for (FDoor& door : RoomDoors[idRandomRoom].Doors) {
+			if (door.dir == reverseDir) {
+				idCurrentRoom = idRandomRoom;
+				isPositiveVectorTeleport = (reverseDir == EDirection::TOP || reverseDir == EDirection::RIGHT);
+				return door;
+			}
+		}
+	}
+	
+	return FDoor();
+}
